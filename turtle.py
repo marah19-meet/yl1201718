d789d34e68a37eb1ac202f67c7dc9cb07f506935
nicole18-meet <kazakov.nicole@gmail.com>
2017-10-29 18:57:07 +0200

    my first commit

---
+++ turtle.py
@@ -0,0 +1,13 @@
+import turtle
+turtle.hideturtle()
+turtle.color("red")
+turtle.pensize(5)
+
+for i in range(4):
+	turtle.forward(100)
+	turtle.left(90)
+	turtle.forward(100)
+	turtle.left(90)
+turtle.mainloop()
+
+
