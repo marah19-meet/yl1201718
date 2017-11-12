04a529bb0304b5958e221e5b623ea4a892342873
nicole18-meet <kazakov.nicole@gmail.com>
2017-11-12 18:56:27 +0200

    lol

--- lab3.py
+++ lab3.py
@@ -1,2 +1,16 @@
 import turtle
-turtle.shape("")
+#turtle.speed(100)
+#angle=5
+#for i in range(361):
+#	turtle.forward(100)
+#	turtle.right(50)
+	#turtle.forward(50)
+	#turtle.left(10)
+	#turtle.forward(30)
+	#turtle.home()
+	#turtle.right(angle)
+	#angle = angle+1
+#turtle.mainloop()
+turtle.addshape("burger.gif")
+turtle.shape("burger.gif")
+turtle.mainloop()
