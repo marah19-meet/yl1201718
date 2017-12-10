61eb59e46fb8b312ba264d0f43683f33999a7410
nicole18-meet <kazakov.nicole@gmail.com>
2017-12-10 19:09:04 +0200

    lol

---
+++ lab6num2.py
@@ -0,0 +1,12 @@
+from turtle import *
+import random
+import math
+
+class rectangle(Turtle):
+	def __init__(self,height,speed,color,width):
+		Turtle.__init__(self)
+		self.shape("rectangle")
+		self.shapesize(10)
+		self.color(color)
+		self.speed(speed)
+ 
